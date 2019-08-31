2fcd1e73271808085be8a613ae73c2910104813b
devibrahem <dev.ibrahembastawi@gmail.com>
2019-08-31 12:21:31 +0200

    setting up webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,19 @@
 import express from "express";
 import { join } from "path";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 import open from "open";
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  })
+);
 
 app.get("/", function(req, res) {
   res.sendFile(join(__dirname, `../src/index.html`));
